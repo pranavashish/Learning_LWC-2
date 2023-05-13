76654e1fbde895e2c0aad8a36cae37155c957106
pranavashish <pranavashish@gmail.com>
2023-05-14 00:32:10 +0530

    Learning Setters

--- force-app/main/default/lwc/setter/setter.js
+++ force-app/main/default/lwc/setter/setter.js
@@ -1,3 +1,10 @@
 import { LightningElement } from 'lwc';
 
-export default class Setter extends LightningElement {}
+export default class Setter extends LightningElement {
+    userDetails = {
+        name:"Gopal",
+        age: 15
+        
+        
+    }
+}
