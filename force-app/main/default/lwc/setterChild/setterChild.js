65ce796d643cbfaba4ab6a40979a6ef90d91dc92
pranavashish <pranavashish@gmail.com>
2023-05-14 00:41:27 +0530

    Setter Done

--- force-app/main/default/lwc/setterChild/setterChild.js
+++ force-app/main/default/lwc/setterChild/setterChild.js
@@ -1,3 +1,13 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api} from 'lwc';
 
-export default class SetterChild extends LightningElement {}
+export default class SetterChild extends LightningElement {
+    userDetail
+    @api 
+    get detail(){
+        return this.userDetail
+    }
+    set detail(data){
+        let newAge = data.age = data.age * 2 
+        this.userDetail = {...data, age:newAge}
+    }
+}
