3ef1f1adf42ea59575dae1facd2bdf27b359d571
pranavashish <pranavashish@gmail.com>
2023-05-15 14:44:24 +0530

    Third Party JS

--- force-app/main/default/lwc/setterChild/setterChild.js
+++ force-app/main/default/lwc/setterChild/setterChild.js
@@ -1,13 +1,13 @@
 import { LightningElement, api} from 'lwc';
 
 export default class SetterChild extends LightningElement {
-    userDetail
-    @api 
-    get detail(){
-        return this.userDetail
-    }
-    set detail(data){
-        let newAge = data.age = data.age * 2 
-        this.userDetail = {...data, age:newAge}
-    }
+    // userDetail
+    // @api 
+    // get detail(){
+    //     return this.userDetail
+    // }
+    // set detail(data){
+    //     let newAge = data.age = data.age * 2 
+    //     this.userDetail = {...data, age:newAge}
+    // }
 }
